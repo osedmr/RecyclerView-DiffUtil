31bba337d52ddb4bf525678c18e50a6a228575d4
osman <osefe47@gmail.com>
2024-10-25 15:44:22 +0300

    RecyclerView & ItemTouchHelper

---
+++ app/src/main/java/com/example/recyclerview/Meals.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerview
+
+data class Meals(var name:String, var mail:String, var password:String)
---
+++ app/src/main/java/com/example/recyclerview/MyAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.databinding.CardViewBinding
+
+class MyAdapter(var cartList:List<Meals>) : RecyclerView.Adapter<MyAdapter.CartViewHolder>() {
+    class CartViewHolder(var binding:CardViewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val binding = CardViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return CartViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+       return cartList.size
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        holder.binding.name.text = cartList[position].name
+        holder.binding.mail.text = cartList[position].mail
+        holder.binding.password.text = cartList[position].password
+    }
+}
